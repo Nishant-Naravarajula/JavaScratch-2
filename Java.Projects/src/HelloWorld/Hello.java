23005ab31e52bab5eb6eb635ff2b32f4001a3fd1
Nishh <72911890+Nishant-Naravarajula@users.noreply.github.com>
2023-02-04 13:29:22 +0530

    Third commit

---
+++ Java.Projects/src/HelloWorld/Hello.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+import java.util.*;
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name : ");
+		String a = sc.nextLine();
+		System.out.println("Hello "+a);
+	}
+
+}
