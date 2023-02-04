01114371e6aa1eda4c821d0d489107a4b3f6cbfd
Nishh <72911890+Nishant-Naravarajula@users.noreply.github.com>
2023-02-04 12:26:22 +0530

    First Commit

---
+++ Java.Projects/src/HelloWorld/MailingAddress.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+public class MailingAddress {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nishant.N");
+		System.out.println("Address here.");
+		System.out.println("500049");
+	}
+
+}
